850e0ffd286d146ec3644f2d0cd22aff63474676
Jaurock <aurockjsoe@gmail.com>
2016-06-04 10:05:05 -0600

    creando menu

--- app/src/main/java/com/example/jrock/mywarehouse/MainActivity.java
+++ app/src/main/java/com/example/jrock/mywarehouse/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jrock.mywarehouse;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            // lo ideal aquí sería hacer un intent para abrir una nueva clase como lo siguiente
+           // Intent about = new Intent(getApplicationContext(), About.class);
+            //startActivity(about);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
